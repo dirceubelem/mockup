f1a916f99ed2f0d98b966b24a62427cbf430c967
Dirceu Belém <dirceu@fourtime.com>
2019-05-07 13:35:33 -0300

    Subindo Banner

--- src/main/java/fourti/me/mobkup/api/ApplicationConfig.java
+++ src/main/java/fourti/me/mobkup/api/ApplicationConfig.java
@@ -26,6 +26,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
 
 //        resources.add(ServiceAccount.class);
+        resources.add(ServiceBanner.class);
         resources.add(ServiceProduct.class);
         resources.add(ServiceVersion.class);
 
